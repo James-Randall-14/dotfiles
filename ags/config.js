25da8d65df92b6fc591eb2a770a109e9786b3af6
James Randall <jgrandall@proton.me>
2024-05-11 16:01:10 +0000

    started working on ags

--- ags/config.js
+++ ags/config.js
@@ -1,5 +1,15 @@
+const Bar = (monitor = 0) => Widget.Window({
+    monitor,
+    name: `bar${monitor}`,
+    anchor: ['top', 'left', 'right'],
+    child: Widget.Label()
+        .poll(1000, label => label.label = Utils.exec('date')),
+})
+
 App.config({
     windows: [
+      Bar(0),
+      Bar(1),
         // this is where window definitions will go
     ]
 })
